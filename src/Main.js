5b65857a13db918a4a1e13d45d31bebe87cc75ff
Jalil <benharkatdjalil@gmail.com>
2022-08-10 16:54:56 +0400

    Updated to use Web3model

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,25 @@
-import Category from './pages/Category';
-import Home from './pages/Home';
-import { Routes, Route } from 'react-router-dom';
-import Project from './pages/Project';
-import Programs from './pages/Programs';
-import About from './pages/About';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Category from "./pages/Category";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Project from "./pages/Project";
+import Programs from "./pages/Programs";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Signup from "./pages/SignupPage";
 
 export default function Main() {
-  return (
-    <div className="main">
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/home" element={<Home/>} />
-        <Route path="/category/:categoryname" element={<Category />} />
-        <Route exact path="/project/:id" element={<Project />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/programs" element={<Programs />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
-      </Routes>
-    </div>
-  )
+	return (
+		<div className="main">
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/home" element={<Home />} />
+				<Route path="/category/:categoryname" element={<Category />} />
+				<Route exact path="/project/:id" element={<Project />} />
+				<Route exact path="/about" element={<About />} />
+				<Route exact path="/programs" element={<Programs />} />
+				<Route exact path="/login" element={<Login />} />
+				<Route exact path="/signup" element={<Signup />} />
+			</Routes>
+		</div>
+	);
 }
-

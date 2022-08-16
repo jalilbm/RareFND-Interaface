b56f70664fc8c3bc672f7428bd59c3508314fd0b
Jalil <benharkatdjalil@gmail.com>
2022-08-17 00:42:08 +0400

    Singup v0

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -11,7 +11,7 @@ export default function CategoryCarousel(props) {
 					src={props.image}
 					alt="Third slide"
 					style={{
-						height: "50vh",
+						height: `50${window.innerHeight > window.innerWidth ? "vw" : "vh"}`,
 						minHeight: "300px",
 						objectFit: "cover",
 						filter: "blur(3px)",
@@ -22,7 +22,7 @@ export default function CategoryCarousel(props) {
 					<h1
 						className="display-1"
 						style={{
-							color:"white",
+							color: "white",
 							fontFamily: "'Cinzel', serif",
 							fontSize: "10vw",
 						}}
